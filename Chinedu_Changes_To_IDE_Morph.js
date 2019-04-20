86bc345821d527e56abb7c7e56f7c016f95e8920
chinedu amadi-ndukwe <chinedua@umich.edu>
2019-04-20 17:25:22 -0400

    still working

--- Chinedu_Changes_To_IDE_Morph.js
+++ Chinedu_Changes_To_IDE_Morph.js
@@ -4,6 +4,7 @@ IDE_Morph.prototype.addCustom3DPolygonBlock = function (){
     // One way is to create the polygon mesh from this function and add it to the stage as a costume but I will
     // need to figure out how any GUI blocks I create will interact with this polygon
     // Another way is to import an exported threejs/editor geometry/shape as an obj or via json
+    // https://threejs.org/docs/#api/en/geometries/IcosahedronGeometry
 
     console.log("We will add a custom polygon block to the sprite from this function");
 
