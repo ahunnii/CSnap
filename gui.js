a8d7fc96ae60217b9452b1267a6488228778a0ee
Andrew Hunn <ahunn@umich.edu>
2019-10-22 16:23:14 -0400

    Load order potential fix

--- gui.js
+++ gui.js
@@ -81,6 +81,34 @@ var WardrobeMorph;
 var SoundIconMorph;
 var JukeboxMorph;
 
+// Costumes
+var Costumes_2D = {
+    "Social Justice Icons": {
+        "directory": "socialjustice",
+        "src": ["breatTheseChains.png","dontShoot.png", "dove.png", "dreads2.png", "fist.png", "heart.png", "hipHop.png", "libertyBell.png", "micHand.png", "peace2.png", "quincunx.png", "scaleOfJustice.png", "write.png"]
+    },
+    "Anishinaabe": {
+        "directory": "anishinaabe",
+        "src": ["berry.png","bigpetal.png","bud1.png","flower1.png","flower2.png", "leaf1.png","medicinewheel.png", "petal.png", "petal2.png", "rhombus.png"]
+    },
+	"Gee's Bend": {
+        "directory": "gees",
+        "src": ["gb_unit1.png","gb_unit2.png","stripe.png","stripes.png","stripes3.png"]
+    },
+	"Lakota": {
+        "directory": "lakota",
+        "src": ["rhombus.png","rhombus1.png"]
+    },
+	"Appalachian": {
+        "directory": "appalachian",
+        "src": ["blond.png","green.png", "plaid.png", "rose.png", "white.png"]
+    },
+	"Four Directions": {
+        "directory": "fourdirections",
+        "src": ["head.png","human.png", "kite.png", "medicinewheel.png", "rhombus1.png", "rhombus2.png", "triangle1.png", "triangle2.png", "turtle_island.png", "turtlefoot.png", "turtlehead.png"]
+    },
+}
+
 // IDE_Morph ///////////////////////////////////////////////////////////
 
 // I am SNAP's top-level frame, the Editor window
@@ -2800,7 +2828,7 @@ IDE_Morph.prototype.projectMenu = function () {
         menu.addItem(
             '2D ' + localize(graphicsName) + '...',
             function () {
-    
+            
                 let directory = config.asset_path + graphicsName,
 
                 names = myself.getCostumesList2D(); // Passing in a url or in this
